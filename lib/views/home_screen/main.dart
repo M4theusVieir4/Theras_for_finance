f0a078bb0e374763940cf93280538944080d2400
unknown <vieiramatheus72@gmail.com>
2023-11-03 01:02:10 -0300

    adicionando about us

--- lib/views/home_screen/main.dart
+++ lib/views/home_screen/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:theras_app/views/menu_empresas/footer.dart';
 import '../menu_empresas/main.dart';
 import '../details_screen/main.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -11,7 +12,7 @@ class HomeScreen extends StatelessWidget {
 }
 
 Widget _buildColumn(BuildContext context) => Container(
-    height: 2000,
+    height: 3000,
     child: Column(
       //crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -21,6 +22,7 @@ Widget _buildColumn(BuildContext context) => Container(
         _buildMidContainerWithButton(context),
         // Bottom white container
         _buildBottomContainer(),
+        _buildTopAbout(),
         Footer()
       ],
     ));
@@ -31,7 +33,9 @@ Widget _buildTopContainer(context) => Flexible(
         decoration: BoxDecoration(
           //gradient: LinearGradient(colors: [Color(0xFFC86DD7), Color(0xFF3023AE)]),
           color: Color.fromRGBO(113, 99, 255, 1),
-          borderRadius: BorderRadius.only(bottomRight: Radius.circular(70)),
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(150),
+              bottomLeft: Radius.circular(150)),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -196,19 +200,163 @@ Widget _buildMidContainerWithButton(BuildContext context) {
   );
 }
 
+final List<String> imageFileNames = [
+  "assets/company_imagens/azul.png",
+  "assets/company_imagens/itub.png",
+  "assets/company_imagens/b3sa.png",
+  "assets/company_imagens/cash.png",
+  "assets/company_imagens/flry.png",
+  "assets/company_imagens/mglu.png",
+  "assets/company_imagens/itsa.png",
+  "assets/company_imagens/petz.png",
+  "assets/company_imagens/sqia.png",
+  "assets/company_imagens/sanb.png",
+  "assets/company_imagens/asai.png",
+  "assets/company_imagens/enju.png",
+  "assets/company_imagens/eztc.png",
+  "assets/company_imagens/alpa.png",
+  "assets/company_imagens/ccro.png",
+  "assets/company_imagens/petr.png",
+  "assets/company_imagens/lren.png",
+  "assets/company_imagens/lwsa.png",
+  "assets/company_imagens/ntco.png",
+  "assets/company_imagens/mtre.png"
+// Adicione mais nomes de arquivos conforme necessário
+];
 Widget _buildBottomContainer() => Flexible(
-      flex: 5,
+      flex: 2,
       child: Container(
         color: Color.fromRGBO(250, 250, 250, 1),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
-            'Bottom container',
-            style: TextStyle(
-              fontSize: 17.0,
-              fontWeight: FontWeight.w600,
-              color: Colors.black54,
+              '''Trabalhamos com análises das maiores empresas da bolsa brasileira''',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                height: 1.9,
+                fontWeight: FontWeight.w900,
+                color: Colors.black,
+              )),
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Container(
+              width: 1700, // Largura desejada para o Container
+              height: 200, // Altura desejada para o Container
+              child: ImageCarousel(imageFileNames: imageFileNames),
             ),
           ),
+        ]),
+      ),
+    );
+
+class ImageCarousel extends StatelessWidget {
+  final List<String> imageFileNames;
+
+  ImageCarousel({required this.imageFileNames});
+
+  @override
+  Widget build(BuildContext context) {
+    final groupsOf4 = <List<String>>[];
+    for (int i = 0; i < imageFileNames.length; i += 4) {
+      groupsOf4.add(imageFileNames.sublist(i, i + 4));
+    }
+
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 100, // Altura do carrossel (mesmo que a altura do Container)
+        viewportFraction: 1.0, // Exibe um único item por vez
+        autoPlay: true, // Ativar a reprodução automática
+        autoPlayInterval: Duration(seconds: 4), // Intervalo de 4 segundos
+        autoPlayAnimationDuration:
+            Duration(milliseconds: 800), // Duração da animação
+        autoPlayCurve: Curves.fastOutSlowIn, // Curva de animação
+        // Defina a margem desejada entre os itens
+      ),
+      items: groupsOf4.map((group) {
+        return Row(
+          children: group.map((fileName) {
+            return Expanded(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 80),
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        fileName), // Carrega a imagem usando o nome do arquivo
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        );
+      }).toList(),
+    );
+  }
+}
+
+Widget _buildTopAbout() => Flexible(
+      flex: 2,
+      child: Container(
+        color: Color.fromRGBO(250, 250, 250, 1),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Text(
+            'Sobre Nós',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 45,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              SizedBox(width: 40),
+              Text(
+                '''Sistema de apoio para \ninvestidores''',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              SizedBox(width: 40),
+              Text(
+                  '''Somos estudantes de engenharia da computação, estudamos na faculdade UNIP - Indianópolis e alguns integrantes está no 7º e 8º semestre.\n A Theras é um projeto do nosso trabalho anual do curso de engenharia da computação, onde temos que desenvolver um Sistema de Apoio\n a Decisão (SAD).A ideia é desenvolver um sistema que ajude na tomada de decisão voltada para o mercado financeiro.''',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 15,
+                    height: 1.7,
+                    color: Colors.black,
+                  )),
+              SizedBox(width: 80),
+              Transform.scale(
+                scale: 1,
+                child: Image.asset(
+                  "assets/company_imagens/therasYTB.jpeg",
+                  width: 700,
+                  height: 700,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Center(),
         ]),
       ),
     );
